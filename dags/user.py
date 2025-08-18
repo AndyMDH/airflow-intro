5c08fecaa4503b1ed551f2420b1997ad12095f60
Andy Ho <andy.hohuu.96@gmail.com>
2025-08-18 20:00:26 +0200

    Enhance user.py: Import Context and add user_location asset for user data retrieval

--- dags/user.py
+++ dags/user.py
@@ -1,4 +1,4 @@
-from airflow.sdk import asset
+from airflow.sdk import asset, Asset, Context
 
 @asset(
     schedule="@daily",
@@ -10,4 +10,15 @@ def user(self) -> dict[str]:
     r = requests.get(self.uri)
     return r.json()
     
-
+@asset(
+    schedule=user,
+    
+    
+)
+def user_location(user: Asset, context: Context) -> dict[str]:
+    user_data = context['ti'].xcom_pull(
+        dag_id=user.name,
+        task_id=user.name,
+        include_prior_dates=True
+        )
+    return user_data['results'][0]['location']
