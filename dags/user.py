4cbfccba1cfbeae922b6c322496ae461ab224a27
Andy Ho <andy.hohuu.96@gmail.com>
2025-08-20 21:26:40 +0200

    Refactor docker-compose.yaml to switch Airflow executor to CeleryExecutor, enable Redis service, and add airflow-worker and flower services. Remove test_format.py and add new celery.py DAG for task orchestration.

--- dags/user.py
+++ dags/user.py
@@ -9,31 +9,12 @@ def user(self) -> dict[str]:
     return r.json()
 
 
-@asset.multi(outlets=[Asset(name="user_location"), Asset(name="user_login")])
+@asset.multi(
+    schedule=user, outlets=[Asset(name="user_location"), Asset(name="user_login")]
+)
 def user_info(user: Asset, context: Context) -> list[dict[str]]:
     user_data = context["ti"].xcom_pull(
         dag_id=user.name,
         task_ids=user.name,
-        include_prior_dates=True,
     )
     return [user_data["results"][0]["location"], user_data["results"][0]["login"]]
-
-
-@asset(schedule=user)
-def user_location(user: Asset, context: Context) -> dict[str]:
-    user_data = context["ti"].xcom_pull(
-        dag_id=user.name,
-        task_ids=user.name,
-        include_prior_dates=True,
-    )
-    return user_data["results"][0]["location"]
-
-
-@asset(schedule=user)
-def user_login(user: Asset, context: Context) -> dict[str]:
-    user_data = context["ti"].xcom_pull(
-        dag_id=user.name,
-        task_ids=user.name,
-        include_prior_dates=True,
-    )
-    return user_data["results"][0]["login"]
--- test_format.py
+++
@@ -1,6 +0,0 @@
-def test_function(x, y):
-    return x + y
-
-
-if __name__ == "__main__":
-    test_function(1, 2)
