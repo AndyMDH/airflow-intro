6d56ff1c477fbdc4eee8ae8e37a5f1d130407cbc
Andy Ho <andy.hohuu.96@gmail.com>
2025-08-22 14:17:47 +0200

    add custom sql decorator

--- my-sdk/my_sdk/decorators/sql.py
+++ my-sdk/my_sdk/decorators/sql.py
@@ -1,30 +1,64 @@
-from typing import Sequence, ClassVar
-from airflow.decorators import DecoratedOperator
+from typing import Any, ClassVar, Collection, Mapping, Callable
+from collections.abc import Sequence
+from airflow.sdk.bases.decorator import DecoratedOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sdk.definitions._internal.types import SET_DURING_EXECUTION
+from airflow.utils.context import context_merge
+from airflow.utils.operator_helpers import determine_kwargs
+from airflow.sdk.definitions.context import Context
+import warnings
 
 
-class _SQLDecoratedClass(DecoratedOperator, SQLExecuteQueryOperator):
-    template_field: Sequence[str] = (
-        (*DecoratorOperator.template_field, *SQLExecuteQueryOperator.template_field),
+class _SQLDecoratedOperator(DecoratedOperator, SQLExecuteQueryOperator):
+
+    template_fields: Sequence[str] = (
+        *DecoratedOperator.template_fields,
+        *SQLExecuteQueryOperator.template_fields,
     )
-    template_field_renderers: ClassVar[dict[str, str]] = {
-        *DecoratorOperator.template_field_renderer,
-        **SQLExecuteQueryOperator.template_field_renderer
+    template_fields_renderers: ClassVar[dict[str, str]] = {
+        **DecoratedOperator.template_fields_renderers,
+        **SQLExecuteQueryOperator.template_fields_renderers,
     }
 
-    custom_operator_name: str = "task.sql"
+    custom_operator_name: str = "@task.sql"
     overwrite_rtif_after_execution: bool = True
-    
-    def __init__(self, 
-                 *, 
-                 callable: Callable, 
-                 ops_args: Collection[Any] | None = None,
-                 ops_kwags: Collection[Any] | None = None,
-                 **kwargs) -> None: 
-        
+
+    def __init__(
+        self,
+        *,
+        python_callable: Callable,
+        op_args: Collection[Any] | None = None,
+        op_kwargs: Mapping[str, Any] | None = None,
+        **kwargs,
+    ) -> None:
+
         if kwargs.pop("multiple_outputs", None):
-            warning.warn(
-                f"multiple_output=True is not supported for {self.__class__.__name__}. Ignoring."
+            warnings.warn(
+                f"`multiple_outputs=True` is not supported in {self.custom_operator_name} tasks. Ignoring.",
                 UserWarning,
-                stacklevel=3
+                stacklevel=3,
             )
+
+        super().__init__(
+            python_callable=python_callable,
+            op_args=op_args,
+            op_kwargs=op_kwargs,
+            sql=SET_DURING_EXECUTION,
+            multiple_outputs=False,
+            **kwargs,
+        )
+
+    def execute(self, context: Context) -> Any:
+        context_merge(context, self.op_kwargs)
+        kwargs = determine_kwargs(self.python_callable, self.op_args, context)
+
+        self.sql = self.python_callable(*self.op_args, **kwargs)
+
+        if not isinstance(self.sql, str) or self.sql.strip() == "":
+            raise TypeError(
+                "The returned value from the TaskFlow callable must be a non-empty string."
+            )
+
+        context["ti"].render_templates()
+
+        return super().execute(context)
